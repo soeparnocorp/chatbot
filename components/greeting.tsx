4bb7e2fe78d49c801b4c69356375307b7a65454d
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-01-15 21:06:34 +0700

    greeting.tsx

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -22,7 +22,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.6 }}
       >
-        How can I help you today?
+        How can Anna Laura AI help you today?
       </motion.div>
     </div>
   );
